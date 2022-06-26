5bbdf2f8fb0a3bf5cef0b267d896ade9e3d1eb35
ichizokichinosuke <nuuuukemw@gmail.com>
2022-06-26 19:30:35 +0900

    modify gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Sample Gatsby.js Main by gatsby-starter-default`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@kenshiro.ata`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
