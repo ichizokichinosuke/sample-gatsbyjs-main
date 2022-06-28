ef7a858228281c52463f18dda984ffac34f0dd87
ichizokichinosuke <nuuuukemw@gmail.com>
2022-06-29 00:23:21 +0900

    ckpt 0629

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,3 +7,17 @@ exports.createPages = async ({ actions }) => {
     defer: true,
   })
 }
+
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
